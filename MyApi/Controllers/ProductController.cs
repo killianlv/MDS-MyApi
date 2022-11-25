78bd5eb5bac0cabac2af6cd79e98eb413f9b49a4
Killian LE VAILLANT <killian.levaillant@exfabrica.io>
2022-11-25 14:36:57 +0100

    API Summary & Description

--- MyApi/Controllers/ProductController.cs
+++ MyApi/Controllers/ProductController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using MyApi.Services.ProductService;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace MyApi.Controllers
 {
@@ -20,6 +21,9 @@ namespace MyApi.Controllers
 
         
         [HttpGet]
+        [SwaggerOperation(Summary ="Get all products")]
+        [ProducesResponseType(typeof(IList<Product>), 200)]
+        [ActionName("GetAllProducts")]
         public async Task<ActionResult<List<Product>>> GetAllProducts()
         {
             var result = await _productService.GetAllProducts();
@@ -28,6 +32,11 @@ namespace MyApi.Controllers
 
         
         [HttpGet("id")]
+        [SwaggerOperation(Summary = "Get a product", Description = "Pass product id as parameter")]
+        [ProducesResponseType(typeof(Product), 200)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ActionName("GetSigleProduct")]
         public async Task<ActionResult<Product>> GetSigleProduct(int id)
         {
             if (!ModelState.IsValid)
@@ -43,6 +52,10 @@ namespace MyApi.Controllers
 
         
         [HttpPost]
+        [SwaggerOperation(Summary = "Add a new product", Description = "Pass the product as parameter")]
+        [ProducesResponseType(typeof(List<Product>), 200)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ActionName("AddProduct")]
         public async Task<ActionResult<List<Product>>> AddProduct(Product product)
         {
             if (!ModelState.IsValid)
@@ -55,6 +68,11 @@ namespace MyApi.Controllers
 
         
         [HttpPut("id")]
+        [SwaggerOperation(Summary = "Update a product", Description = "Pass product id as parameter and update product")]
+        [ProducesResponseType(typeof(List<Product>), 200)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ActionName("UpdateProduct")]
         public async Task<ActionResult<List<Product>>> UpdateProduct(int id, Product requets)
         {
             if (!ModelState.IsValid)
@@ -70,6 +88,10 @@ namespace MyApi.Controllers
         
         
         [HttpDelete("id")]
+        [SwaggerOperation(Summary = "Delete a product", Description = "Pass product id as parameter")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult> DeleteProduct(int id)
         {
             if (!ModelState.IsValid)
--- MyApi/Program.cs
+++ MyApi/Program.cs
@@ -14,6 +14,8 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddDbContext<DataContext>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddSwaggerGen(c => { c.EnableAnnotations(); });
+
 
 
 var app = builder.Build();
