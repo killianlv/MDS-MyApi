6a4ca15184d132cae79623a535666dd78ed2b7fe
Killian LE VAILLANT <killian.levaillant@exfabrica.io>
2022-12-14 09:47:04 +0100

    Add Code

--- MyApi/Entity/ProductEntity.cs
+++ MyApi/Entity/ProductEntity.cs
@@ -7,5 +7,6 @@ namespace MyApi.Entity
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public int Stock { get; set; }
+        public long code { get; set; }
     }
 }
--- MyApi/Models/Product.cs
+++ MyApi/Models/Product.cs
@@ -5,5 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public int Stock { get; set; }
+        public long code { get; set; }
+
     }
 }
